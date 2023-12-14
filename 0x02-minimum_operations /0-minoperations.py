c67688ff881928050d74e7512c50326e96654d85
theyasflower <84896487+theyasflower@users.noreply.github.com>
2023-12-14 23:58:14 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations /0-minoperations.py
+++ 0x02-minimum_operations /0-minoperations.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
-""" Module for 0-minoperations"""
+
+"""
+    Method that determines the number of minmum operations given n characters
+"""
 
 
 def minOperations(n):
     """
-    minOperations
-    Gets fewest # of operations needed to result in exactly n H characters
+        A function that calculates the fewest number of operations
+        needed to give a result of exactly n H characters in a file
+        args: n: Number of characters to be displayed
+        return:
+               number of min operations
     """
-    # all outputs should be at least 2 char: (min, Copy All => Paste)
-    if (n < 2):
-        return 0
-    ops, root = 0, 2
-    while root <= n:
-        # if n evenly divides by root
-        if n % root == 0:
-            # total even-divisions by root = total operations
-            ops += root
-            # set n to the remainder
-            n = n / root
-            # reduce root to find remaining smaller vals that evenly-divide n
-            root -= 1
-        # increment root until it evenly-divides n
-        root += 1
-    return ops
+
+    now = 1
+    start = 0
+    counter = 0
+    while now < n:
+        remainder = n - now
+        if (remainder % now == 0):
+            start = now
+            now += start
+            counter += 2
+        else:
+            now += start
+            counter += 1
+    return counter
